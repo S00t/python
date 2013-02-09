7828ad67c7a27075f58aca5488db3f1806fb0f13
root <root@Dojo.(none)>
2013-02-09 23:29:32 +0100

    Quick Sort

--- quick_sort.py
+++ quick_sort.py
@@ -1,33 +1,31 @@
 from random import randrange
 
 
-N = raw_input("Podaj ile liczba w tablicy : ")
+N = int(raw_input("Podaj ile liczba w tablicy : "))
 
-def convert(n):
-  try:
-    ret = int(n)
-  except ValueError:
-    ret = float(n)
-  return ret
-N = convert(N)  
-    
+
 list = []
 for i in range(N):
   i = randrange(1,N)	  
   list.append(i)
-#print Temp
 
-def quick(list):
+def quick(list,counter=0):
   if list == []:
     return []
   else:
     pivot = list[0]
-    L = quick([x for x in list[1:] if x < pivot])
-    R = quick([x for x in list[1:] if x >= pivot])
     
-    return L + [pivot] + R
+    L = quick([x for x in list[1:] if x < pivot])
+     
+    R = quick([x for x in list[1:]  if x >= pivot])
+    
+    counter = counter + 1
+    
 
+    return L + [pivot] + R
+  
 d = list   
-print "Przed posortowaniem : ", d, #len(d)
+print "Przed posortowaniem : \n", d,"\n" 
 g = quick(d)
-print "Po posortowaniu : ",  g, # len(g)
+print "Po posortowaniu : \n",  g,"\n"
+print "liczba przestawien w L %d"%(len(g))
